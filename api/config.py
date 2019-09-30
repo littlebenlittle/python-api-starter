f6685986460773a73965e9867f4626479c868a13
littlebenlittle <ben.little6@gmail.com>
2019-09-30 13:51:51 -0600

    ssl enables (fragile)

--- api/config.py
+++ api/config.py
@@ -1,5 +1,30 @@
 
 import os
 import sys
+from types import SimpleNamespace
 
-pass
+_config_vars = [
+    'VAULT_ADDR',
+    'VAULT_TOKEN',
+    'CLIENT_SECRETS_FILE',
+    'OAUTH2_SCOPES',
+    'FLASK_SECRET',
+    'OAUTH2_REDIRECT',
+]
+
+_missing = []
+for var in _config_vars:
+    if not os.environ.get(var):
+        _missing.append(var)
+
+if any(_missing):
+    print('Missing environment variables:')
+    for var in _missing:
+        print(var)
+    sys.exit(1)
+
+c = SimpleNamespace()
+for var in _config_vars:
+    setattr(c, var, os.environ[var])
+
+c.OAUTH2_SCOPES = c.OAUTH2_SCOPES.split(',')
--- api/server.py
+++ api/server.py
@@ -1,14 +1,57 @@
 
-from config import *
+import hvac
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import flask
+from config import c
 from flask import Flask, request
 
 app = Flask(__name__)
+app.secret_key = c.FLASK_SECRET
+vault_client = hvac.Client(
+    url=c.VAULT_ADDR,
+    token=c.VAULT_TOKEN,
+)
 
 
-@app.route('/ping', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        print('received a get request')
-    else:
-        print(request.json())
-    return b'success', 200
+@app.route('/auth', methods=['GET'])
+def auth():
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        c.CLIENT_SECRETS_FILE,
+        c.OAUTH2_SCOPES,
+    )
+    flow.redirect_uri = c.OAUTH2_REDIRECT
+    authorization_url, state = flow.authorization_url(
+        access_type='offline',
+        include_granted_scopes='true',
+    )
+    flask.session['state'] = state
+    return flask.redirect(authorization_url)
+
+
+@app.route('/oauth2callback', methods=['GET'])
+def oauth2callback():
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        c.CLIENT_SECRETS_FILE,
+        c.OAUTH2_SCOPES,
+        state=flask.session['state']
+    )
+    flow.redirect_uri = c.OAUTH2_REDIRECT
+    authorization_response = flask.request.url
+    flow.fetch_token(authorization_response=authorization_response)
+    path = f'gpc/user/{flask.session["user"]}/oauth2credentials'
+    store(path=path, secret=flow.credentials)
+
+
+def store(path, secret):
+    rep = vault_client.secrets.kv.v2.create_or_update_secret(
+        path=path,
+        secret=secret,
+    )
+
+
+def read(path):
+    rep = vault_client.secrets.kv.read_secret_version(
+        path=path
+    )
+    return rep['data']['data']
